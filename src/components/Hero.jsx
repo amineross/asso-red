be0f029f05f5227882be060cfe574f0aa51fcb07
Amine Ross <amine.rostane@outlook.com>
2023-11-23 12:01:34 +0100

    Navbar: progress on dropdown menu

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+
 
 const Hero = () => {
   return (
     <div className='text-white'>
-        <div className=' w-[80%] h-full mt-[200px] mx-auto text-left flex flex-col justify-center'>
+        <div className=' w-[70%] h-full mt-[200px] mx-auto text-left flex flex-col justify-center'>
             <p className=' font-homepage text-7xl font-black'>
                 RESEARCH<br/>
                 EXPERIMENTATION<br/>
                 DEPARTMENT
             </p>
+
+            <div className='flex my-4'>
+            <FaInstagram className=' scale-[2] mx-2 text-left text-[#C91B0E] cursor-pointer'/>
+            <FaLinkedinIn className=' scale-[2] mx-2 ml-6 text-left text-[#C91B0E] cursor-pointer' />
+            </div>
         </div>
     </div>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import Logo from '../assets/Logo.svg'
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+
 
 const NavBar = () => {
   return (
-    <div className=' text-gray-300 flex justify-between items-center h-24 w-full max-w-[1240px] mx-auto px-4'>
-        <ul className='z-10 flex justify-between w-[90%] mx-auto mt-[40px] text-xl'>
-            <li className='p-4 hover:text-[#C91B0E] cursor-pointer'>Association</li>
-            <li className='p-4 hover:text-[#C91B0E] cursor-pointer'>Local</li>
-            <img src={Logo} alt="RED Logo" className=' scale-75'/>
-            <li className='p-4 hover:text-[#C91B0E] cursor-pointer'>Bureau</li>
-            <li className='p-4 hover:text-[#C91B0E] cursor-pointer'>Projets</li>
-        </ul>
+    <div className='text-gray-300 flex text-right'>
+        <div className=' text-gray-300 flex justify-between items-center h-24 w-full max-w-[1240px] mx-auto px-4'>
+            <ul className='z-10 flex justify-between w-[90%] mx-auto mt-[40px] text-xl'>
+                <li className='p-4 hover:text-[#C91B0E] cursor-pointer hidden'>Association</li>
+                <li className='p-4 hover:text-[#C91B0E] cursor-pointer hidden'>Local</li>
+                <img src={Logo} alt="RED Logo" className=' scale-75'/>
+                <li className='p-4 hover:text-[#C91B0E] cursor-pointer hidden'>Bureau</li>
+                <li className='p-4 hover:text-[#C91B0E] cursor-pointer hidden'>Projets</li>
+            </ul>
+            
+        </div>
+        <div className='z-10 px-4 mx-10 mt-[40px] cursor-pointer'>
+            <AiOutlineMenu size={40}/>
+        </div>
     </div>
+    
   )
 }
 
