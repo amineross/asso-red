fee4ad2c267337d798c772c4b5cb2e826840f934
Amine ROSTANE <72439293+amineross@users.noreply.github.com>
2024-02-27 19:40:37 +0100

    Updated: NewsCard clickable

--- src/components/News/NewsCard.jsx
+++ src/components/News/NewsCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import {BsCheckLg} from 'react-icons/bs'
 import NewsImg from '../../assets/macbook.png'
 import NewsAuthorImg from '../../assets/members/member_1.png'
@@ -6,34 +7,35 @@ import NewsAuthorImg from '../../assets/members/member_1.png'
 const NewsCard = ({ className }) => {
   return (
     <div className={`rounded-xl overflow-hidden cursor-pointer shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] ${className}`}>
-        {/* THIS ONE IMAGE */}
-        <img src={NewsImg} alt="title" className='w-full object-cover object-center h-auto'/>
+        <Link to={"/news/1"}>    
+            <img src={NewsImg} alt="title" className='w-full object-cover object-center h-auto'/>
 
-        <div className='p-5'>
-        <h2 className=' font-homepage font-bold text-xl text-gray-900'>Article title</h2>
-            <p className=' mt-3 text-sm text-gray-700'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-            <div className='flex justify-between flex-nowrap items-center mt-6'>
-                <div className='flex items-center gap-x-2'>
-                    <img src={NewsAuthorImg} alt="Author" className='rounded-full mr-2 w-[35px]'/>
-                    <div className='flex flex-col'>
-                        <h4 className='font-bold italic text-gray-900 text-sm'>
-                            Cartelot Adrien
-                        </h4>
-                        <div className='flex items-center gap-x-2'>
-                            <span className='bg-[#36B37E] w-fit bg-opacity-20 p-1.5 rounded-full'>
-                                <BsCheckLg className='w-2 h-2 text-[#36B37E]'/>
-                            </span>
-                            <span className='italic text-gray-600 text-xs'>
-                                Président
-                            </span>
+            <div className='p-5'>
+            <h2 className=' font-homepage font-bold text-xl text-gray-900'>Article title</h2>
+                <p className=' mt-3 text-sm text-gray-700'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+                <div className='flex justify-between flex-nowrap items-center mt-6'>
+                    <div className='flex items-center gap-x-2'>
+                        <img src={NewsAuthorImg} alt="Author" className='rounded-full mr-2 w-[35px]'/>
+                        <div className='flex flex-col'>
+                            <h4 className='font-bold italic text-gray-900 text-sm'>
+                                Cartelot Adrien
+                            </h4>
+                            <div className='flex items-center gap-x-2'>
+                                <span className='bg-[#36B37E] w-fit bg-opacity-20 p-1.5 rounded-full'>
+                                    <BsCheckLg className='w-2 h-2 text-[#36B37E]'/>
+                                </span>
+                                <span className='italic text-gray-600 text-xs'>
+                                    Président
+                                </span>
+                            </div>
                         </div>
                     </div>
+                    <span className='font-bold'>25 Dec</span>
                 </div>
-                <span className='font-bold'>25 Dec</span>
             </div>
-        </div>
+        </Link>
     </div>
   )
 }
